83d79f67898469755a459fbbe0a526278382027a
OTHMAN\othman <othman.subhi.123@gmail.com>
2020-10-21 12:29:17 +0300

    fixing some stuff

--- src/Dot.java
+++ src/Dot.java
@@ -2,19 +2,31 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Dot {
-	int [] position = { 680 , 250 } ;
-	int directions_num ;
-	ArrayList<int[]> direction_set = new ArrayList<int[]>();
+	int [] position = { 680 , 250 };
+	int max_steps;
+	ArrayList<int[]> direction_set;
 	int steps;
 	Dot(){
-		directions_num= new Random().nextInt(20);
-		for (int i =0 ; i <directions_num ; i++) {
+		steps = 0;
+		max_steps = 1000 ;
+		max_steps= new Random().nextInt(20);
+		direction_set = new ArrayList<int[]>();
+		for (int i =0 ; i < max_steps ; i++) {			
 			int [] direction = new int[2];
 			position[0] =new Random().nextInt(5);
 			position[1] =new Random().nextInt(5);
 			direction_set.add(direction);
 		}
-		
-		
+	}
+	
+	
+	public Dot inheriteFromDad(){
+		Dot baby = new Dot();
+		baby.position=this.position;
+		baby.max_steps= this.max_steps;
+		for(int[] dir : direction_set) {
+			baby.direction_set.add(dir);
+		}
+		return baby;	
 	}
 }
