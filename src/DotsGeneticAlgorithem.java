5c6e923c10b086061fd5041800ff288d7478aa73
OTHMAN\othman <othman.subhi.123@gmail.com>
2020-10-24 23:05:45 +0300

    GUI done

--- src/DotsGeneticAlgorithem.java
+++ src/DotsGeneticAlgorithem.java
@@ -13,13 +13,19 @@ import javax.swing.SwingConstants;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.awt.SystemColor;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.JCheckBox;
 
 public class DotsGeneticAlgorithem extends JFrame {
 
 	private JPanel contentPane;
 	private Population pop;
-	private JLabel gen;
 	int framerate;
+	private JTextField textField_1;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -37,7 +43,7 @@ public class DotsGeneticAlgorithem extends JFrame {
 	
 	public DotsGeneticAlgorithem() {
 		setType(Type.UTILITY);
-		setTitle("DotsGA");
+		setTitle("Amazy Mazy");
 		setResizable(false);
 		setBounds(100, 100 , 700 , 700);
 		contentPane = new JPanel();
@@ -45,29 +51,59 @@ public class DotsGeneticAlgorithem extends JFrame {
 		setContentPane(contentPane);
 		framerate=60;
 		contentPane.setLayout(null);
-		gen = new JLabel("Generation :");
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Auto next generation");
+		chckbxNewCheckBox.setFont(new Font("Segoe Print", Font.BOLD | Font.ITALIC, 12));
+		chckbxNewCheckBox.setHorizontalAlignment(SwingConstants.CENTER);
+		chckbxNewCheckBox.setBounds(485, 339, 199, 23);
+		contentPane.add(chckbxNewCheckBox);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(UIManager.getColor("CheckBox.light"));
+		panel_1.setBounds(485, 117, 199, 117);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel gen = new JLabel("Generation :");
+		gen.setBounds(0, 11, 199, 58);
 		gen.setHorizontalAlignment(SwingConstants.CENTER);
 		gen.setFont(new Font("Segoe Print", Font.BOLD | Font.ITALIC, 28));
-		gen.setBounds(498, 59, 186, 38);
-		contentPane.add(gen);
+		gen.setBackground(Color.BLACK);
+		panel_1.add(gen);
+		
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Segoe Print", Font.PLAIN, 15));
+		textField_1.setEditable(false);
+		textField_1.setColumns(10);
+		textField_1.setBounds(13, 70, 176, 36);
+		panel_1.add(textField_1);
+		
+		JButton btnNewButton_1 = new JButton("Reset Maze");
+		btnNewButton_1.setFont(new Font("Segoe Print", Font.BOLD | Font.ITALIC, 20));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(485, 608, 199, 35);
+		contentPane.add(btnNewButton_1);
 		
 		JPanel panel = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
-		panel.setBackground(Color.GREEN);
+		panel.setBackground(new Color(139, 69, 19));
 		panel.setBounds(25, 25, 450, 620);
 		contentPane.add(panel);
 		
-		JLabel background = new JLabel("New label");
-		background.setIcon(new ImageIcon(DotsGeneticAlgorithem.class.getResource("/imgs/mazebackground.jpg")));
-		background.setBounds(0, 0, 694, 671);
-		contentPane.add(background);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(569, 333, 89, 23);
+		JButton btnNewButton = new JButton("Next generation");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(485, 277, 199, 35);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(566, 372, 89, 23);
-		contentPane.add(btnNewButton_1);
+		JLabel background = new JLabel("New label");
+		background.setIcon(new ImageIcon(DotsGeneticAlgorithem.class.getResource("/imgs/mazebackground.jpg")));
+		background.setBounds(0, 0, 700, 700);
+		contentPane.add(background);
 	}
 }
