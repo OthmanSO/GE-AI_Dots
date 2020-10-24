b61ffd5f9598a888f73114f1318989d7d6baa748
OTHMAN\othman <othman.subhi.123@gmail.com>
2020-10-25 01:14:43 +0300

    document listener need urgent fix  class DotsGeneticAlgorithem

--- src/DotsGeneticAlgorithem.java
+++ src/DotsGeneticAlgorithem.java
@@ -5,6 +5,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import java.awt.Color;
 import javax.swing.JDesktopPane;
 import java.awt.Window.Type;
@@ -15,6 +18,9 @@ import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import java.awt.SystemColor;
 import java.awt.event.ActionListener;
+import java.awt.event.InputMethodListener;
+import java.lang.reflect.Array;
+import java.util.EventObject;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
@@ -25,11 +31,11 @@ public class DotsGeneticAlgorithem extends JFrame {
 	private JPanel contentPane;
 	private Population pop;
 	private int framerate;
-	private JTextField textField_1;
+	private JTextField gencount;
 	private MazeGenerator mg;
 	private JTextField samplesnum;
 	
-	
+	static Integer lastint;
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -48,7 +54,7 @@ public class DotsGeneticAlgorithem extends JFrame {
 		setType(Type.UTILITY);
 		setTitle("Amazy Mazy");
 		setResizable(false);
-		setBounds(100, 100 , 700 , 700);
+		setBounds(100, 100 , 1000 ,700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -59,20 +65,50 @@ public class DotsGeneticAlgorithem extends JFrame {
 		samplesnum.setFont(new Font("Segoe Print", Font.BOLD | Font.ITALIC, 15));
 		samplesnum.setHorizontalAlignment(SwingConstants.CENTER);
 		samplesnum.setText("50");
-		samplesnum.setBounds(542, 609, 66, 35);
+		samplesnum.setBounds(815, 610, 66, 35);
 		contentPane.add(samplesnum);
 		samplesnum.setColumns(3);
+		samplesnum.getDocument().addDocumentListener( new DocumentListener() {
+			
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				chkdigit(e);
+			}
+			
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				// TODO Auto-generated method stub
+				chkdigit(e);
+			}
+			
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				chkdigit(e);
+			}
+			
+			public void chkdigit(DocumentEvent e){
+				JTextField tf = (JTextField)  ((EventObject) e).getSource();
+				int x= 0;
+				try {
+				x = Integer.parseInt(tf.getText());
+				}catch(Exception notInt) {
+					tf.setText(DotsGeneticAlgorithem.lastint.toString());
+				}
+				DotsGeneticAlgorithem.lastint = x ;
+			}
+		}
+		);
 		
 		JCheckBox chckbxNewCheckBox = new JCheckBox("Auto next generation");
 		chckbxNewCheckBox.setBackground(SystemColor.info);
 		chckbxNewCheckBox.setFont(new Font("Segoe Print", Font.BOLD | Font.ITALIC, 12));
 		chckbxNewCheckBox.setHorizontalAlignment(SwingConstants.CENTER);
-		chckbxNewCheckBox.setBounds(470, 339, 199, 23);
+		chckbxNewCheckBox.setBounds(740, 339, 199, 23);
 		contentPane.add(chckbxNewCheckBox);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(SystemColor.info);
-		panel_1.setBounds(470, 118, 199, 117);
+		panel_1.setBounds(740, 108, 199, 117);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
 		
@@ -83,13 +119,13 @@ public class DotsGeneticAlgorithem extends JFrame {
 		gen.setBackground(Color.BLACK);
 		panel_1.add(gen);
 		
-		textField_1 = new JTextField();
-		textField_1.setBackground(SystemColor.info);
-		textField_1.setFont(new Font("Segoe Print", Font.PLAIN, 15));
-		textField_1.setEditable(false);
-		textField_1.setColumns(10);
-		textField_1.setBounds(13, 70, 176, 36);
-		panel_1.add(textField_1);
+		gencount = new JTextField();
+		gencount.setBackground(SystemColor.info);
+		gencount.setFont(new Font("Segoe Print", Font.PLAIN, 15));
+		gencount.setEditable(false);
+		gencount.setColumns(4);
+		gencount.setBounds(13, 70, 176, 36);
+		panel_1.add(gencount);
 		
 		JButton btnNewButton_1 = new JButton("Reset Maze");
 		btnNewButton_1.setBackground(new Color(245, 222, 179));
@@ -99,14 +135,14 @@ public class DotsGeneticAlgorithem extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton_1.setBounds(470, 563, 199, 35);
+		btnNewButton_1.setBounds(740, 564, 199, 35);
 		contentPane.add(btnNewButton_1);
 		
 		JPanel panel = new JPanel();
 		panel.setToolTipText("samples count");
 		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
 		panel.setBackground(new Color(139, 69, 19));
-		panel.setBounds(10, 25, 450, 620);
+		panel.setBounds(30, 25, 640, 620);
 		contentPane.add(panel);
 		
 		JButton btnNewButton = new JButton("Next generation");
@@ -115,13 +151,13 @@ public class DotsGeneticAlgorithem extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(470, 278, 199, 35);
+		btnNewButton.setBounds(740, 272, 199, 35);
 		contentPane.add(btnNewButton);
 		
 		JLabel background = new JLabel("New label");
 		background.setBackground(new Color(240, 240, 240));
 		background.setIcon(new ImageIcon(DotsGeneticAlgorithem.class.getResource("/imgs/mazebackground.jpg")));
-		background.setBounds(0, 0, 700, 700);
+		background.setBounds(0, 0, 1042, 700);
 		contentPane.add(background);
 	}
 	
