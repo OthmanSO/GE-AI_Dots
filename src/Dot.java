35db940eb379429fc7e2ae596a9358e384f5da2f
SabahTARTIR365 <sosotarteer2017@gmail.com>
2020-10-26 17:40:03 +0300

    some implementation for function

--- src/Dot.java
+++ src/Dot.java
@@ -6,11 +6,13 @@ public class Dot {
 	int max_steps;
 	ArrayList<Integer> direction_set;
 	int steps;
+	boolean dead,reach;
 	Dot(){
 		steps = 0;
 		max_steps = 1000 ;
 		direction_set = new ArrayList<Integer>();
-		for (int i =0 ; i < max_steps ; i++) {			
+		for (int i =0 ; i < max_steps ; i++) {	
+			
 			// up down right left
 		}
 	}
@@ -29,6 +31,36 @@ public class Dot {
 	// mutation function  
 	public void mutate() {
 		
+		  double mutationRate = 0.01;//chance that any vector in directions gets changed
+		    for (int i =0; i< direction_set.size(); i++) {
+		    	Random random =new Random();
+		      float rand = random.nextInt(1);
+		      if (rand < mutationRate) {
+		        //set this direction as a random direction 
+		        float randomAngle = random.nextInt(4);
+		      //  direction_set.add(i,  PVector.fromAngle(randomAngle));
+		      }
+		    }
+		
 	}
 	
+	
+	
+	public boolean isDead() {
+		
+		if(dead)
+		return true;
+		else return false ;// not yet implemented
+		
+		
+	}
+	
+public boolean isREACH() {
+		
+		if(reach)
+		return true;
+		else return false ;// not yet implemented
+		
+		
+	}
 }
