a26a6d2d19d2c3516bd4698b897612b095ab56f5
OTHMAN\othman <othman.subhi.123@gmail.com>
2020-10-26 17:46:47 +0200

    formatting

--- src/Target.java
+++ src/Target.java
@@ -1,9 +1,9 @@
 
 public class Target {
-	int [] position = { 250 , 10 };
-	
-	 static int[] distanceFromTarget(int pos[]) {
-		 int[] distance = {Math.abs(pos[0]-250) , Math.abs(pos[1]-10)};
-		 return distance;
-	 }
+	int[] position = { 250, 10 };
+
+	static int[] distanceFromTarget(int pos[]) {
+		int[] distance = { Math.abs(pos[0] - 250), Math.abs(pos[1] - 10) };
+		return distance;
+	}
 }
