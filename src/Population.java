c2ba138600e12061977eaf05337d2bb18a1c70c9
SabahTARTIR365 <sosotarteer2017@gmail.com>
2020-11-11 00:20:18 +0300

    adding bee

--- src/Population.java
+++ src/Population.java
@@ -3,7 +3,8 @@ public class Population {
 
 	Dot Dots_population[];
 	Dot bestdotever;
-
+   int indexOFBestDot=0;
+ public static int MaxStep=1000;
 	public Population(int pop) {
 		Dots_population = new Dot[pop];
 		for (int i = 0; i < Dots_population.length; i++) {
@@ -24,13 +25,51 @@ public class Population {
 	// - if more than one has the same fitness the one that occurs first will be
 	// token -
 	public void myPromisingBaby() {
+Target target ;
+float max = 0;
+int maxIndex = 0;
+for (int i = 0; i<Dots_population.length; i++) {
+	
+	if(Dots_population[i].isReached()) {
+		
+		if(Dots_population[i].max_steps<MaxStep) {
+		MaxStep=Dots_population[i].max_steps; 
+		
+		indexOFBestDot=i;
+		bestdotever =Dots_population[indexOFBestDot];}
+		
+	
+	}//if reach 
+	
+	
+		else {
+			
+			
+		//	if (Dots_population[i].fitness > max) {
+			   // max = Dots_population[i].fitness;
+			  //  maxIndex = i;
+			 // }
+		
+	}	
 
-	}
+}//for
+
+}
 
 	// mutation function that iterates on all dots in the population and delegate it
 	// to the mutation in dot class
 	public void mutation() {
 
 	}
+	
+	 boolean allDead() {
+		    for (int i = 0; i< Dots_population.length; i++) {
+		      if (!Dots_population[i].isDead() && !Dots_population[i].isReached()) { 
+		        return false;
+		      }
+		    }
+
+		    return true;
+		  }
 
 }
