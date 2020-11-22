4acb4df89dbfa872df1d207bd83ec2e32f7d704f
SabahTARTIR365 <60351703+SabahTARTIR365@users.noreply.github.com>
2020-11-22 23:28:55 +0300

    onother generate

--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -1,3 +1,5 @@
+import java.security.SecureRandom;
+import java.util.Random;
 
 public class MazeGenerator {
 
@@ -6,18 +8,60 @@ public class MazeGenerator {
 	int rows = 32; // number of rows of cells in maze, including a wall around edges
 	int columns = 32; // number of columns of cells in maze, including a wall around edges
 
-	final static int wallCode = 0;
-	final static int pathCode = 1;
 
+
+	   final static int backgroundCode = 0;
+	    final static int wallCode = 1;
+	    final static int pathCode = 2;
+	    final static int emptyCode = 3;
+	    final static int visitedCode = 4;
+	    
+	    
+	    
+	    int n=30;
+	    private boolean[][] north;     // is there a wall to north of cell i, j
+	    private boolean[][] east;
+	    private boolean[][] south;
+	    private boolean[][] west;
+	    private boolean[][] visited;
+	    private boolean done = false;
 	public MazeGenerator() {
 		maze = new boolean[32][32];
 		maz = new int[32][32];
+		
+		
+		
+		//-------
+		 // initialize border cells as already visited
+        visited = new boolean[n+2][n+2];
+        for (int x = 0; x < n+2; x++) {
+            visited[x][0] = true;
+            visited[x][n+1] = true;
+        }
+        for (int y = 0; y < n+2; y++) {
+            visited[0][y] = true;
+            visited[n+1][y] = true;
+        }
 
+
+        // initialze all walls as present
+        north = new boolean[n+2][n+2];
+        east  = new boolean[n+2][n+2];
+        south = new boolean[n+2][n+2];
+        west  = new boolean[n+2][n+2];
+        for (int x = 0; x < n+2; x++) {
+            for (int y = 0; y < n+2; y++) {
+                north[x][y] = true;
+                east[x][y]  = true;
+                south[x][y] = true;
+                west[x][y]  = true;
+            }
+        }
 	}
 
 
 	// ---------------------------------------------------------
-
+/*
 	void generate() {
 		// Create a random maze. The strategy is to start with
 		// a grid of disconnected "rooms" separated by walls,
@@ -60,6 +104,15 @@ public class MazeGenerator {
 			wallrow[r] = wallrow[i];
 			wallcol[r] = wallcol[i];
 		}
+		
+		for (i=1; i<rows-1; i++)  // replace negative values in maze[][] with emptyCode
+            for (j=1; j<columns-1; j++)
+                if (maz[i][j] < 0)
+                    maz[i][j] = emptyCode;
+        synchronized(this) {
+            try { wait(1000); }
+            catch (InterruptedException e) { }
+        }
 
 	}
 
@@ -81,30 +134,102 @@ public class MazeGenerator {
 			maz[row][col] = maz[row + 1][col];
 
 		}
-		setMaze();
-	}
+	
+	}*/
 //----------------------------------------------------
+	// generate the maze
+    private void generate(int x, int y) {
+        visited[x][y] = true;
 
-	public void setMaze() {
-		for (int i = 0; i < 32; i++) {
-			for (int j = 0; j < 32; j++) {
+        // while there is an unvisited neighbor
+        while (!visited[x][y+1] || !visited[x+1][y] || !visited[x][y-1] || !visited[x-1][y]) {
 
-				if (maz[i][j] == 0) {
-					maze[i][j] = false;
-				} else
-					maze[i][j] = true;
-			}
-		}
-	}
+            // pick random neighbor (could use Knuth's trick instead)
+            while (true) {
+            	Random random = new Random();
+            int	r = random.nextInt(4);
+                if (r == 0 && !visited[x][y+1]) {
+                    north[x][y] = false;
+                    south[x][y+1] = false;
+                    generate(x, y + 1);
+                    break;
+                }
+                else if (r == 1 && !visited[x+1][y]) {
+                    east[x][y] = false;
+                    west[x+1][y] = false;
+                    generate(x+1, y);
+                    break;
+                }
+                else if (r == 2 && !visited[x][y-1]) {
+                    south[x][y] = false;
+                    north[x][y-1] = false;
+                    generate(x, y-1);
+                    break;
+                }
+                else if (r == 3 && !visited[x-1][y]) {
+                    west[x][y] = false;
+                    east[x-1][y] = false;
+                    generate(x-1, y);
+                    break;
+                }
+            }
+        }
+    }
 
+    // generate the maze starting from lower left
+    public void generate() {
+        generate(30,30);
+
+/*
+        // delete some random walls
+        for (int i = 0; i < n; i++) {
+            int x = 1 + StdRandom.uniform(n-1);
+            int y = 1 + StdRandom.uniform(n-1);
+            north[x][y] = south[x][y+1] = false;
+        }
+
+        // add some random walls
+        for (int i = 0; i < 10; i++) {
+            int x = n/2 + StdRandom.uniform(n/2);
+            int y = n/2 + StdRandom.uniform(n/2);
+            east[x][y] = west[x+1][y] = true;
+        }
+*/
+     
+    }
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//\**************************************************************************************/\\
 
 	public static boolean isAWall(int row, int col) {
 		try {
-			return maze[row][col];
-			// if ( maze[row][col] == (int that agreed it is wall )return true ;
+			//return maze[row][col];
+			if ( maz[row][col] == 1  )return true ;
 		} catch (ArrayIndexOutOfBoundsException e) {
 			return true;
 		}
+		return false;
 	}
 
 	public static boolean isDeadByMaze(int[] position) {
@@ -112,3 +237,11 @@ public class MazeGenerator {
 	}
 
 }
+
+
+
+
+
+
+
+
