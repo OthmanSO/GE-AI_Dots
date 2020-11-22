35037fd4adbbac80cfdb1e8dd130ad1d89b48683
OTHMAN\othman <othman.subhi.123@gmail.com>
2020-11-22 20:04:52 +0200

    ..

--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -2,7 +2,7 @@
 public class MazeGenerator {
 
 	static boolean[][] maze;
-	int[][] maz;
+	static int[][] maz;
 	private static MazeGenerator mg = new MazeGenerator();
 	int rows = 32; // number of rows of cells in maze, including a wall around edges
 	int columns = 32; // number of columns of cells in maze, including a wall around edges
@@ -13,55 +13,62 @@ public class MazeGenerator {
 	public MazeGenerator() {
 		maze = new boolean[32][32];
 		maz = new int[32][32];
-		for (int i = 0; i < 32; i++) {
-			for (int j = 0; j < 32; j++) {
-				// if(i==j) maze[i][j]=true;
-				maze[i][j] = false;
-			}
-		}
-		maze[0][0] = true;
-		maze[0][1] = true;
-		maze[31][31] = true;
 
-	}
-
-	public static void main(String[] args) {
-		System.out.print("hi print \n");
-		MazeGenerator m = new MazeGenerator();
-		// m.generate();
-		m.printMaz();
-
-	}
-
-	// ---------------------------------------------------------
-	void generate() {
 	}
 
 	/*
-	 * void generate() { // Create a random maze. The strategy is to start with // a
-	 * grid of disconnected "rooms" separated by walls, // then look at each of the
-	 * separating walls, in a random // order. If tearing down a wall would not
-	 * create a loop // in the maze, then tear it down. Otherwise, leave it in
-	 * place. int i,j; int emptyCt = 0; // number of rooms int wallCt = 0; // number
-	 * of walls int[] wallrow = new int[(rows*columns)/2]; // position of walls
-	 * between rooms int[] wallcol = new int[(rows*columns)/2]; for (i = 0; i<rows;
-	 * i++) // start with everything being a wall for (j = 0; j < columns; j++)
-	 * maz[i][j] = wallCode; for (i = 1; i<rows-1; i += 2) { // make a grid of empty
-	 * rooms for (j = 1; j<columns-1; j += 2) { emptyCt++; maz[i][j] = -emptyCt; //
-	 * each room is represented by a different negative number if (i < rows-2) { //
-	 * record info about wall below this room wallrow[wallCt] = i+1; wallcol[wallCt]
-	 * = j; wallCt++; } if (j < columns-2) { // record info about wall to right of
-	 * this room wallrow[wallCt] = i; wallcol[wallCt] = j+1; wallCt++; } } }
-	 * 
-	 * 
-	 * int r; for (i=wallCt-1; i>0; i--) { r = (int)(Math.random() * i); // choose a
-	 * wall randomly and maybe tear it down tearDown(wallrow[r],wallcol[r]);
-	 * wallrow[r] = wallrow[i]; wallcol[r] = wallcol[i]; }
-	 * 
-	 * 
+	 * public static void main(String[] args) { System.out.print("hi print \n");
+	 * MazeGenerator m = new MazeGenerator(); // m.generate(); m.printMaz();
 	 * 
 	 * }
 	 */
+
+	// ---------------------------------------------------------
+
+	void generate() {
+		// Create a random maze. The strategy is to start with
+		// a grid of disconnected "rooms" separated by walls,
+		// then look at each of theseparating walls, in a random
+		// order. If tearing down a wall would not create a loop
+		// in the maze, then tear it down. Otherwise, leave it in place.
+		int i, j;
+		int emptyCt = 0;
+		// number of rooms
+		int wallCt = 0;
+		// number of walls
+		int[] wallrow = new int[(rows * columns) / 2]; // position of walls between rooms
+		int[] wallcol = new int[(rows * columns) / 2];
+		for (i = 0; i < rows; i++)
+			// start with everything being a wall
+			for (j = 0; j < columns; j++)
+				maz[i][j] = wallCode;
+		for (i = 1; i < rows - 1; i += 2) { // make a grid of empty rooms
+			for (j = 1; j < columns - 1; j += 2) {
+				emptyCt++;
+				maz[i][j] = -emptyCt;
+				// each room is represented by a different negative number
+				if (i < rows - 2) { // record info about wall below this room
+					wallrow[wallCt] = i + 1;
+					wallcol[wallCt] = j;
+					wallCt++;
+				}
+				if (j < columns - 2) { // record info about wall to right of this room
+					wallrow[wallCt] = i;
+					wallcol[wallCt] = j + 1;
+					wallCt++;
+				}
+			}
+		}
+
+		int r;
+		for (i = wallCt - 1; i > 0; i--) {
+			r = (int) (Math.random() * i); // choose a wall randomly and maybe tear it down
+			tearDown(wallrow[r], wallcol[r]);
+			wallrow[r] = wallrow[i];
+			wallcol[r] = wallcol[i];
+		}
+
+	}
 
 	void tearDown(int row, int col) {
 		// Tear down a wall, unless doing so will form a loop. Tearing down a wall
